5a6678cf19eed6edb41c224848e452382c0f08cb
Javier Lona <javierlona1@gmail.com>
2018-08-13 15:51:18 -0500

    getting back api response

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 const JOKE_BUTTON = document.querySelector("#get-jokes-btn");
 
-JOKE_BUTTON.addEventListener("click", get_jokes);
+JOKE_BUTTON.addEventListener("click", get_jokes, false);
 
 function get_jokes(event){
-  console.log(1);
+  const NUMBER = document.querySelector("input[type='number']").value;
   
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${NUMBER}`, true);
+
+  xhr.onload = function(){
+    if(this.status === 200){
+      let response = this.responseText;
+      console.log(response);
+    }
+  }
+  xhr.send();
   event.preventDefault();
-}
+  }
