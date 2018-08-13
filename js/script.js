e59138714c93d17af58826673f23d8f94bb49549
Javier Lona <javierlona1@gmail.com>
2018-08-13 16:33:05 -0500

    displaying jokes on the front page

--- js/script.js
+++ js/script.js
@@ -6,12 +6,29 @@ function get_jokes(event){
   const NUMBER = document.querySelector("input[type='number']").value;
   
   let xhr = new XMLHttpRequest();
-  xhr.open('GET', `http://api.icndb.com/jokes/random/${NUMBER}`, true);
+  xhr.open("GET", `http://api.icndb.com/jokes/random/${NUMBER}`, true);
 
   xhr.onload = function(){
     if(this.status === 200){
+      // Get back JSON object
       let response = this.responseText;
-      console.log(response);
+      // Convert to JS object
+      let jsObj = JSON.parse(response);
+      console.log(jsObj);
+
+      let output = "";
+      console.log(jsObj.type);
+      
+      if(jsObj.type === "success"){
+        console.log(jsObj.value.joke);
+        jsObj.value.forEach(function(joke){
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output += "Request error."
+      }
+      document.querySelector("#jokes").innerHTML = output;
+
     }
   }
   xhr.send();
